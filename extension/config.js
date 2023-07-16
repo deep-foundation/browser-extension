461db7f047eb642a31b2e483ba47281092fcc935
Roman <technopriest666@gmail.com>
2023-07-17 00:39:40 +0300

    config fix

---
+++ extension/config.js
@@ -0,0 +1,2 @@
+export const GQL_URL = "https://3006-deepfoundation-dev-oy1ilrjd1yk.ws-eu101.gitpod.io/gql"
+export const GQL_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwczovL2hhc3VyYS5pby9qd3QvY2xhaW1zIjp7IngtaGFzdXJhLWFsbG93ZWQtcm9sZXMiOlsiYWRtaW4iXSwieC1oYXN1cmEtZGVmYXVsdC1yb2xlIjoiYWRtaW4iLCJ4LWhhc3VyYS11c2VyLWlkIjoiMzc2In0sImlhdCI6MTY3OTkzNTMyOH0.LmDNulSTSSrm7gKno3E1sBLAhz5TKi-SFBl9oFNfs-k"
